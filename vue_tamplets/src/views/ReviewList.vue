5ba161aa7af86c1b4787175be0c728128fc4552d
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2024-06-28 14:55:06 +0900

    Docs: 추가. 리뷰 리스트 페이지 뷰 변환 끝

--- vue_tamplets/src/views/ReviewList.vue
+++ vue_tamplets/src/views/ReviewList.vue
@@ -1,11 +1,20 @@
 <template>
   <div>
-    <div class="site-name">Travel Destination Recommendation</div>
     <div class="container">
       <SearchBar />
 
       <!-- 리뷰 목록 -->
       <div class="review-list">
+        <div class="input-group mb-3">
+          <input
+            type="text"
+            class="form-control"
+            placeholder="여행지나 리뷰를 검색하세요..."
+          />
+          <div class="input-group-append">
+            <button class="btn btn-outline-primary" type="button">검색</button>
+          </div>
+        </div>
         <ReviewItem
           v-for="review in reviews"
           :key="review.id"
