a19b99e2b3efff53fff30662f499e5f750e0223f
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2024-07-02 10:37:11 +0900

    Feat: 추가. axios 모듈 전역 변수로 선언

--- vue_tamplets/src/main.js
+++ vue_tamplets/src/main.js
@@ -4,8 +4,10 @@ import "bootstrap/dist/css/bootstrap.css";
 import "./styles.css"; // 전역 스타일 추가
 import router from "./router";
 import CKEditor from "@ckeditor/ckeditor5-vue";
+import axios from "axios";
 
 const app = createApp(App);
 app.use(CKEditor);
 app.use(router);
 app.mount("#app");
+app.config.globalProperties.$axios = axios;
