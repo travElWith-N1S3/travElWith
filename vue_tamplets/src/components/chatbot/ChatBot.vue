fbfb650ef7595bba23a026c9ec88604adb95979c
seoHJ99 <121778872+seoHJ99@users.noreply.github.com>
2024-07-03 11:59:52 +0900

    Feat: 추가. 챗봇 접근 유효성 검증 프론트 작업

--- vue_tamplets/src/components/chatbot/ChatBot.vue
+++ vue_tamplets/src/components/chatbot/ChatBot.vue
@@ -56,7 +56,9 @@ export default {
   methods: {
     sendMessage() {
       this.$axios
-        .get("http://localhost:8080/v1/chatbot?prompt=" + this.userMessage)
+        .get(
+          "http://localhost:8080/v1/chatbot/chatting?prompt=" + this.userMessage
+        )
         .then((response) => {
           this.chatHistory.push({
             type: "bot",
@@ -82,10 +84,18 @@ export default {
       const chatBody = this.$refs.chatBody;
       chatBody.scrollTop = chatBody.scrollHeight;
     },
-    closeChat() {
-      // 챗봇 닫기 동작
-      console.log("챗봇을 닫습니다.");
-    },
+  },
+  mounted() {
+    this.$axios.defaults.withCredentials = true;
+    this.$axios.get("http://localhost:8080/v1/chatbot").then((response) => {
+      console.log(response.data);
+      console.log(response);
+      if (response.data == 1) {
+        // alert("연결 성공");
+      } else {
+        alert("유효하지 않은 접근입니다.");
+      }
+    });
   },
 };
 </script>
