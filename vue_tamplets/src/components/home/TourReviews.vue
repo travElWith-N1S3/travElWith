3298f52cd394c30a189100c9d97e76465e6dad5d
sinalol15 <pnsesh01@naver.com>
2024-07-11 11:34:37 +0900

    Docs: 수정. 메인 페이지에서 최신 리뷰 3개 보이도록 수정 및 상세보기 링크 수정

--- vue_tamplets/src/components/home/TourReviews.vue
+++ vue_tamplets/src/components/home/TourReviews.vue
@@ -3,11 +3,15 @@
     <router-link to="/reviews">
       <h2>리뷰글</h2>
     </router-link>
-    <router-link to="/review">
-      <div class="card mb-3" v-for="review in reviews" :key="review.title">
+    <router-link to="/review" v-for="review in reviews" :key="review.id">
+      <div class="card mb-3">
         <div class="card-body">
-          <h5 class="card-title">{{ review.title }}</h5>
-          <p class="card-text">{{ review.text }}</p>
+          <router-link
+            :to="{ name: 'ReviewTour', params: { twReviewNo: review.twReviewNo } }"
+          >
+            <h5 class="card-title">{{ review.twReviewTitle }}</h5>
+            <p class="card-text">{{ stripHtmlTags(review.twReviewContent) }}</p>
+          </router-link>
           <div class="more-link"></div>
         </div>
       </div>
@@ -16,25 +20,41 @@
 </template>
 
 <script>
+import axios from "axios"
+
 export default {
   data() {
     return {
-      reviews: [
-        {
-          id: 1,
-          title: "리뷰 제목 1",
-          text: "이곳에 리뷰 내용이 요약되어 나타납니다. Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque officiis ad dignissimos consectetur, libero necessitatibus sit recusandae! Tempore, alias quas.",
-        },
-        {
-          id: 2,
-          title: "리뷰 제목 2",
-          text: "이곳에 리뷰 내용이 요약되어 나타납니다. Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque officiis ad dignissimos consectetur, libero necessitatibus sit recusandae! Tempore, alias quas.",
-        },
-        // 더 많은 리뷰 데이터 추가 가능
-      ],
-    };
+      reviews: [],
+    }
   },
-};
+  props: {
+    twReviewNo: {
+      type: String,
+      required: true // 필수 속성
+    },
+  },
+  mounted() {
+    this.fetchRecentReviews()
+  },
+  methods: {
+    async fetchRecentReviews() {
+      try {
+        const response = await axios.get("/api1/recentReviews")
+        if (response.data.status) {
+          this.reviews = response.data.recentReviews
+        } else {
+          console.error(response.data.error)
+        }
+      } catch (error) {
+        console.error("Error fetching recent reviews:", error)
+      }
+    },
+    stripHtmlTags(content) {
+      return content.replace(/<\/?p>/g, "")
+    },
+  },
+}
 </script>
 
 <style scoped>
@@ -50,5 +70,13 @@ a {
   position: absolute;
   right: 10px;
   bottom: 10px;
+}
+
+h2 {
+  margin-bottom: 20px;
+}
+
+p {
+  color: black;
 }
 </style>
